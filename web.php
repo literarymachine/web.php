b7aac615211cb4c0980bad0317eebae98b6331bd
Felix Ostrowski <felix.ostrowski@googlemail.com>
2010-01-31 15:00:13 +0100

    Add explicit check for implemented method.

--- web.php
+++ web.php
@@ -6,20 +6,20 @@ class web_app {
         $url = substr($_SERVER['REQUEST_URI'], strlen($_SERVER['SCRIPT_NAME']));
         $method = $_SERVER['REQUEST_METHOD'];
 
-        foreach ($routes as $route => $controller_class) {
+        foreach ($routes as $route => $controller_classname) {
             $escaped_route = str_replace('/', '\/', $route);
             $pattern = preg_replace('/:([^$\\\\]*)/', '([^$\/]+)', $escaped_route);
             $params = array();
             $match = preg_match("/^$pattern$/", $url, $params);
             if (1 === $match) {
                 array_shift($params);
-                try {
-                    $action = new ReflectionMethod($controller_class, $method);
-                    $action->invokeArgs(new $controller_class, $params);
-                    return;
-                } catch (ReflectionException $e) {
+                $controller_class = new ReflectionClass($controller_classname);
+                if (false === $controller_class->hasMethod($method)) {
                     throw new web_exception("Method $method not implemented.");
                 }
+                $action = $controller_class->getMethod($method);
+                $action->invokeArgs($controller_class->newInstance(), $params);
+                return;
             }
         }
 
